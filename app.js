0adf29181a95d0dc66c5f38247a6eb4e46bcfc38
Thamires Telles <thamirestelles12@gmail.com>
2021-06-07 17:37:14 -0300

    Adicionado POST, realizado testes com postman

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req,res) => {
     res.send('Olá, Thamires');
+});
+
+app.get("/josemires", (req,res) => {
+    res.send('Olá, josemires');
+});
+
+app.post("/josemires", (req,res) => {
+    console.log(req.body);
+    console.log(typeof req.body.altura);
+    console.log(typeof req.body.idade);
+    res.send(`Olá, ${req.body.nome} ${req.body.sobrenome} de altura ${req.body.altura}m e na idade ${req.body.idade}`);
 });
 
 app.listen(8080,() => {
